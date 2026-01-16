2e2c70a63cca8db24889b17accfdb27fdc7f0561
Clifford <cliffordchristophermarx@gmail.com>
2026-01-16 20:29:02 +0530

    Another API

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/welcome", (res) => {
+app.get("/welcome", (req, res) => {
   res.send("Welcome to The Express API");
 });
 
